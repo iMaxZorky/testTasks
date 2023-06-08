34e8f9f03775b04aadf277eb2c7b254760efe01a
iMaxZorky <mkoprav@gmail.com>
2023-06-08 10:57:26 +0600

    fix(NoTab): our mission, bi-tool and id-scanner pixel perfect

--- js/index.js
+++ js/index.js
@@ -1,25 +1,40 @@
-const cookieMessage = document.querySelector('.cookie');
-const cookieButton = document.querySelector('.cookie__button');
-
-cookieButton.addEventListener('click', ()=>{
-    cookieMessage.classList.remove('_show');
-});
-setTimeout(() => cookieMessage.classList.add('_show'), 2000);
+const cookieMessage = document.querySelector(".cookie");
+const cookieButton = document.querySelector(".cookie__button");
 
 const feedbackForm = document.forms.feedback;
 const inputs = [...feedbackForm.elements].slice(0, -1);
-
-
-console.log(inputs)
-
 const placeholders = [];
+let emptyFieldsExistance;
+
+cookieButton.addEventListener("click", () => {
+  cookieMessage.classList.remove("_show");
+});
+setTimeout(() => cookieMessage.classList.add("_show"), 2000);
+
 inputs.forEach((input, index) => {
-    placeholders.push(input.placeholder);
-    input.addEventListener("focus", () => {
-            input.placeholder = '';
-        })
-        input.addEventListener("blur", () => {
-            input.placeholder = placeholders[index];
-        })
+  placeholders.push(input.placeholder);
+  input.addEventListener("focus", () => {
+    input.placeholder = "";
+  });
+  input.addEventListener("blur", () => {
+    input.placeholder = placeholders[index];
+  });
+  input.addEventListener("input", () => {
+    input.classList.remove("_empty");
+  });
 });
 
+feedbackForm.addEventListener("submit", function (event) {
+  emptyFieldsExistance = false;
+  event.preventDefault(); //отключаем стандартную отправку формы, чтобы страница не перезагружалась
+  inputs.forEach((input) => {
+    if (!input.value) {
+      input.classList.add("_empty");
+      emptyFieldsExistance = true;
+    }
+  });
+  if (!emptyFieldsExistance) {
+    alert("Form sent");
+    //отправляем форму собственным методом
+  }
+});
