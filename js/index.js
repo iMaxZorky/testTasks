f109e4971b0e7f14edb97e4ff4b819dd0ab4b636
iMaxZorky <mkoprav@gmail.com>
2023-06-19 14:01:09 +0600

    feat(NoTab): add animation in the our mission section

--- js/index.js
+++ js/index.js
@@ -4,7 +4,11 @@ const cookieButton = document.querySelector(".cookie__button");
 const feedbackForm = document.forms.feedback;
 const inputs = [...feedbackForm.elements].slice(0, -1);
 const placeholders = [];
-let emptyFieldsExistance;
+let emptyFieldsExistence;
+
+const ourMissionBlock = document.querySelector('.our-mission')
+const tBlock = document.querySelector('.our-mission__text-block')
+const phoneImage = document.querySelector('.our-mission__phone-image')
 
 cookieButton.addEventListener("click", () => {
   cookieMessage.classList.remove("_show");
@@ -25,16 +29,42 @@ inputs.forEach((input, index) => {
 });
 
 feedbackForm.addEventListener("submit", function (event) {
-  emptyFieldsExistance = false;
+  emptyFieldsExistence = false;
   event.preventDefault(); //отключаем стандартную отправку формы, чтобы страница не перезагружалась
   inputs.forEach((input) => {
     if (!input.value) {
       input.classList.add("_empty");
-      emptyFieldsExistance = true;
+      emptyFieldsExistence = true;
     }
   });
-  if (!emptyFieldsExistance) {
+  if (!emptyFieldsExistence) {
     alert("Form sent");
     //отправляем форму собственным методом
   }
 });
+
+
+
+function offset(el) {
+  const rect = el.getBoundingClientRect(),
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+}
+
+function animationByScroll () {
+  const ourMissionBlockHeight = ourMissionBlock.offsetHeight;
+  const ourMissionBlockOffset = offset(ourMissionBlock).top;
+
+  let animPoint = window.innerHeight - ourMissionBlockHeight;
+
+  if (pageYOffset > (ourMissionBlockOffset - animPoint) && pageYOffset < (ourMissionBlockOffset + ourMissionBlockHeight)) {
+    tBlock.classList.add('_hide');
+    phoneImage.classList.add('_move');
+  } else {
+    tBlock.classList.remove('_hide');
+    phoneImage.classList.remove('_move');
+  }
+}
+
+window.addEventListener('scroll', animationByScroll);
